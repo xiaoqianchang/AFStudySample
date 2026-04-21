e1866274b5108aa1b4ffe50a971c1d57821a558f
xiaochang <xiaochang@abupdate.com>
2026-04-21 15:31:32 +0800

    feat: OOBE-开机向导导航切换实现，避免回到WelcomeActivity

--- NSOOBE/app/src/main/java/com/chang/oobe/android/WelcomeActivity.java
+++ NSOOBE/app/src/main/java/com/chang/oobe/android/WelcomeActivity.java
@@ -15,11 +15,11 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // 需要注释development/apps/SdkSetup/src/com.android.sdksetup/DefaultActivity.java中针对模拟器预制为1的两行代码在放开下面
-//        if (Settings.Secure.getInt(getContentResolver(), Settings.Secure.USER_SETUP_COMPLETE, 0) == 1) {
-//            // 导航切换设置完成会回到该界面，判断如果开机引导设置完成直接关闭进入桌面
-//            Log.e(TAG, "已设置，直接关闭");
-//            finish();
-//        }
+        if (Settings.Secure.getInt(getContentResolver(), Settings.Secure.USER_SETUP_COMPLETE, 0) == 1) {
+            // 导航切换设置完成会回到该界面，判断如果开机引导设置完成直接关闭进入桌面
+            Log.e(TAG, "已设置，直接关闭");
+            finish();
+        }
         setContentView(R.layout.activity_welcome);
 
         findViewById(R.id.btn_next).setOnClickListener(v -> {
